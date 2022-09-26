1405e708cab298c8e818d560515874f4d20fb62f
x <x>
2022-09-26 20:54:57 -0300

    add navbar with logo and styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,15 @@
-const Nav = () => {
-    return (<div>Nav</div>);
+import colorLogo from '../images/logo.png';
+import whiteLogo from '../images/logo_transparent.png';
+
+const Nav = ({ minimal, authToken }) => {
+    return (
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={minimal ? colorLogo : whiteLogo} />
+            </div>
+            {!authToken & !minimal && <button className='nav-button'>Log In</button>}
+        </nav>
+    );
 };
 
 export default Nav;
