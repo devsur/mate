025fcd40acf12ec092aec78c0a42d7637d804b6d
x <x>
2022-09-28 17:41:47 -0300

    add modal skeleton for signup or login

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,16 @@
 import colorLogo from '../images/logo.png';
 import whiteLogo from '../images/logo_transparent.png';
 
-const Nav = ({ minimal, authToken }) => {
+const Nav = ({ minimal, authToken, setShowModal, showModal }) => {
+    const handleClick = () => {
+        setShowModal(true);
+    }
     return (
         <nav>
             <div className="logo-container">
                 <img className="logo" src={minimal ? colorLogo : whiteLogo} />
             </div>
-            {!authToken & !minimal && <button className='nav-button'>Log In</button>}
+            {!authToken & !minimal && <button className='nav-button' onClick={handleClick} disabled={showModal}>Log In</button>}
         </nav>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,26 @@
+import { useState } from 'react';
 import Nav from '../components/Nav';
+import AuthModal from '../components/AuthModal';
 
 const Home = () => {
+    const [showModal, setShowModal] = useState(false);
+
     const authToken = false;
     
     const handleClick = () => {
         console.log('click');
+        setShowModal(true);
     };
     return (
         <div className='overlay'>
-            <Nav minimal={false} authToken={authToken} />
+            <Nav minimal={false} authToken={authToken} setShowModal={setShowModal} showModal={showModal} />
             <div className="home">
                 <h1>Swipe Right</h1>
                 <button
                     className="primary-button"
                     onClick={handleClick}>{authToken ? 'Signout' : 'Create Account'}
                 </button>
+                {showModal && <AuthModal setShowModal={setShowModal} />}
             </div>
         </div>
     );
