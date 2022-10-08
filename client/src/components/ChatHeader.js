33be00cefbea706e83549ef567d086a2cbfdb3e8
x <x>
2022-10-08 20:07:00 -0300

    add get user endpoint, fetch user data in Dashboard and render user data, add login required to dashboard and onboarding views

--- client/src/components/ChatHeader.js
+++ client/src/components/ChatHeader.js
@@ -1,13 +1,21 @@
-const ChatHeader = () => {
+import { useCookies } from 'react-cookie';
+
+const ChatHeader = ({user}) => {
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
+    const logout = () => {
+        removeCookie('UserId', cookies.UserId);
+        removeCookie('AuthToken', cookies.AuthToken);
+        window.location.reload();
+    };
     return (
         <div className="chat-container-header">
             <div className="profile">
                 <div className="img-container">
-                    <img src="" alt="" />
+                    <img src={user.url} alt={`${user.first_name}-photo`} />
                 </div>
-                <h3>Username</h3>
+                <h3>{user.first_name}</h3>
             </div>
-            <i className="log-out-icon">⇦</i>
+            <i className="log-out-icon" onClick={logout}>⇦</i>
         </div>
     );
 }
