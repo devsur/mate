91bdc87facc2722f9424d1da8eb9f8c34c555e1f
x <x>
2022-10-10 19:54:43 -0300

    add list matches and chats with mock chat data

--- client/src/components/MatchesDisplay.js
+++ client/src/components/MatchesDisplay.js
@@ -1,7 +1,36 @@
-const MatchesDisplay = () => {
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const MatchesDisplay = ({matches, setClickedUser}) => {
+    const [matchedProfiles, setMatchedProfiles] = useState(null);
+    const matchedUserIds = matches.map(({user_id}) => user_id);
+    // fetch matched user objects given user ids
+    const getMatches = async () => {
+        try {
+            // send array of matched ids as userIds
+            const response = await axios.get('http://localhost:8000/users', {
+                params: {userIds: JSON.stringify(matchedUserIds)}
+            });
+            setMatchedProfiles(response.data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        getMatches();
+    }, []);
+
     return (
         <div className="matches-display">
-            
+            {matchedProfiles?.map((match, _index) => (
+                <div key={_index} className="match-card" onClick={() => setClickedUser(match)}>
+                    <div className='img-container'>
+                        <img src={match?.url} alt={`${match?.first_name}-profile`} />
+                    </div>
+                    <h3>{match?.first_name}</h3>
+                </div>
+            ))}
         </div>
     );
 }
--- server/index.js
+++ server/index.js
@@ -66,6 +66,32 @@ app.post('/login', async (req, res) => {
     }
 });
 
+// given array of ids, return user objects
+app.get('/users', async (req, res) => {
+    const client = new MongoClient(uri);
+    const userIds = JSON.parse(req.query.userIds);
+    console.log(userIds);
+    try {
+        await client.connect();
+        const database = client.db('app-data');
+        const users = database.collection('users');
+        const pipeline = [
+            {
+                '$match': {
+                    'user_id': {
+                        '$in': userIds
+                    }
+                }
+            }
+        ];
+        const foundUsers = await users.aggregate(pipeline).toArray();
+        console.log(foundUsers);
+        res.send(foundUsers);
+    } finally {
+        await client.close();
+    }
+});
+
 app.get('/gendered-users', async (req, res) => {
     const client = new MongoClient(uri);
     const gender = req.query.gender; 
@@ -139,6 +165,22 @@ app.put('/addmatch', async (req, res) => {
         };
         const user = await users.updateOne(query, updateDocument);
         res.send(user);
+    } finally {
+        await client.close();
+    }
+});
+
+app.get('/messages', async (req, res) => {
+    const client = new MongoClient(uri);
+    const {userId, correspondingUserId} = req.query;
+    console.log(userId, correspondingUserId);
+    try {
+        await client.connect();
+        const database = client.db('app-data');
+        const messages = database.collection('messages');
+        const query = {from_userId: userId, to_userId: correspondingUserId};
+        const foundMessages = await messages.find(query).toArray();
+        res.send(foundMessages);
     } finally {
         await client.close();
     }
