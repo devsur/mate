f9ee905ee315f3aacf4bf370b4063a3560428db5
x <x>
2022-10-10 21:19:57 -0300

    add match users only if right swipe is mutual

--- client/src/components/MatchesDisplay.js
+++ client/src/components/MatchesDisplay.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 const MatchesDisplay = ({matches, setClickedUser}) => {
     const [matchedProfiles, setMatchedProfiles] = useState(null);
+    const [cookies, setCookie, removeCookie] = useCookies(['user']);
     const matchedUserIds = matches.map(({user_id}) => user_id);
+    const userId = cookies.UserId;
     // fetch matched user objects given user ids
     const getMatches = async () => {
         try {
@@ -21,9 +24,12 @@ const MatchesDisplay = ({matches, setClickedUser}) => {
         getMatches();
     }, [/* matches */]); // only get matches if user's matches exist/update
 
+    // add filter to display only actual mutual matches where both users swiped right
+    // filter user.matches for each match if match also includes user's id in their .matches list
+    const filteredMatchedProfiles = matchedProfiles?.filter(matchedProfile => matchedProfile.matches.filter(profile => profile.user_id === userId).length > 0);
     return (
         <div className="matches-display">
-            {matchedProfiles?.map((match) => (
+            {filteredMatchedProfiles?.map((match) => (
                 <div key={match.user_id} className="match-card" onClick={() => setClickedUser(match)}>
                     <div className='img-container'>
                         <img src={match?.url} alt={`${match?.first_name}-profile`} />
